57453fc874f56df8436834c5fd90c9f772d079b7
Maurilio Atila <cabelotaina@gmail.com>
2022-09-21 22:56:00 +0200

    added log using winston and express-winston

--- app.js
+++ app.js
@@ -1,37 +1,60 @@
 const express = require("express");
 const app = express();
-
 const errorHandlerMiddleware = require("./api/middleware/error-handler.js");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const winston = require("winston"),
+  expressWinston = require("express-winston");
 
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
-app.use(bodyParser.json({ limit: '50mb' }))
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-app.use(cookieParser())
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
 
 var corsOptions = {
   exposedHeaders: [
-    'Accept-Language',
-    'Access-Control-Allow-Origin',
-    'Connection', 'Content-Length', 'Content-Type', 'Date',
-    'Etag', 'Server', 'Via', 'X-Powered-By'
-  ]
+    "Accept-Language",
+    "Access-Control-Allow-Origin",
+    "Connection",
+    "Content-Length",
+    "Content-Type",
+    "Date",
+    "Etag",
+    "Server",
+    "Via",
+    "X-Powered-By",
+  ],
 };
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
 require("./api/database");
-const routesApi = require("./api/routes/index");
+const router = require("./api/routes/index");
 
-app.use("/api", routesApi);
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true,
+    msg: "HTTP {{req.method}} {{req.url}}",
+    expressFormat: true,
+    colorize: false,
+    ignoreRoute: function (req, res) {
+      return false;
+    },
+  })
+);
+
+app.use("/api", router);
 
 app.get("/", (req, res) => {
   res.send("Software Warehouse");
 });
 
 app.use("/api/*", function (req, res) {
-  res.status(404).json({ message: "Not found"});
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use(errorHandlerMiddleware);
